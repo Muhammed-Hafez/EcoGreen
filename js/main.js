172a0dc23c71f14502b2fe509882146d0029ef42
mohamed hafez <abdel7afez6@gmail.com>
2024-12-26 15:40:00 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -8,6 +8,71 @@ burgerMenu.addEventListener("click", () => {
   spans.forEach((span) => span.classList.toggle("active"));
 });
 
+// مراقبة العناصر وعند الظهور في الشاشة نضيف الكلاس 'visible'
+const elements = document.querySelectorAll(
+  ".about-head, .about-grid, .about-card, .about-card h3"
+);
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+elements.forEach((element) => {
+  observer.observe(element);
+});
+
+// مراقبة العناصر وعند الظهور في الشاشة نضيف الكلاس 'visible'
+const productGrid = document.querySelector(".product-grid");
+const productCards = document.querySelectorAll(".product-card");
+
+const observerP = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.1 }
+); // 0.1 تعني أن العنصر سيظهر عندما يظهر 10% من مساحته في الشاشة
+
+// مراقبة المصفوفة والبطاقات
+observerP.observe(productGrid);
+productCards.forEach((card) => observerP.observe(card));
+
+$(document).ready(function () {
+  $(".sliderC").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+});
+
 // Smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
